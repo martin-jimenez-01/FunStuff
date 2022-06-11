8aa74afc8aafb47d82cdc0c91574f6b761753b42
Martin Jimenez <96633035+martin-jimenez-01@users.noreply.github.com>
2022-06-11 05:16:07 -0500

    Added a deciphering function

--- Caesar Cipher in Hex/ceaser_in_hex.py
+++ Caesar Cipher in Hex/ceaser_in_hex.py
@@ -5,15 +5,15 @@ Created on Sat Jun  4 18:32:59 2022
 
 @author: muertyn
 """
+from numpy import nan as NAN
 
 # Ceaser Cipher encoder and decoder
-def to_hex(msg):
+def english_to_hex(msg):
     hex_msg = ''
     for i in range(len(msg)):
         hex_msg += format(ord(msg[i]), 'x')
         
     return hex_msg
-
 
 def encoder(msg, key):
     # type checking
@@ -33,9 +33,35 @@ def encoder(msg, key):
         coded_msg += chr(ord(msg[i]) + key)
     
     # convert to hex
-    final_msg = to_hex(coded_msg)
+    final_msg = english_to_hex(coded_msg)
     
     return final_msg
+
+def solver(original_msg, key = NAN):#input no spaces
+    if key == NAN:
+        return "Error: need a key top solve"
+    
+    if len(original_msg) % 2 != 0:
+        return "Error: the original message character count is not an even number. It should be."
+    
+    # convert the hex into decimal
+    deciphered = ''
+    pair = ''
+    i = 0
+    while(i != len(original_msg)):
+        # grab the hex pair 
+        pair = original_msg[i] + original_msg[i+1] 
+        # turn hex into decimal
+        decimal = int(pair, 16)
+        # apply the key backwards
+        decimal -= key
+        # turn into english add to the decoded, final msg
+        deciphered += chr(decimal)
+        # increment i by 2 
+        i += 2 
+        
+    
+    return deciphered 
     
 
         
